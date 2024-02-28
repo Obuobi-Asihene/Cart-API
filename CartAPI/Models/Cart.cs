89b783e1ccee7340dbb8f067f55762fa7bd20bbc
AzureYakubu <obsmediaofficial@gmail.com>
2024-02-28 06:03:49 +0000

    created model for Cart

---
+++ CartAPI/Models/Cart.cs
@@ -0,0 +1,10 @@
+﻿namespace CartAPI.Models
+{
+    public class Cart
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity {  get; set; }
+        public decimal UnitPrice {  get; set; }
+    }
+}
