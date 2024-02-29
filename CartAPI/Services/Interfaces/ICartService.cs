c354716ef83fcad076ef60bb4bdc6cd10d6f57e3
AzureYakubu <obsmediaofficial@gmail.com>
2024-02-29 22:05:31 +0000

    added cart service interface

---
+++ CartAPI/Services/Interfaces/ICartService.cs
@@ -0,0 +1,12 @@
+﻿using CartAPI.Models;
+
+namespace CartAPI.Services.Interfaces
+{
+    public interface ICartService
+    {
+        void AddCartItem(CartDto item);
+        void RemoveCartItem(int itemId);
+        IEnumerable<CartDto> ListCartItems();
+        CartDto GetCartItem(int itemId);
+    }
+}
