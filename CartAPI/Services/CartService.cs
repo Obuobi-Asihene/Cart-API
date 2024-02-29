580ca50366de050749d699d7be7dc35068bb4ea0
AzureYakubu <obsmediaofficial@gmail.com>
2024-02-29 22:22:07 +0000

    added cart service

---
+++ CartAPI/Services/CartService.cs
@@ -0,0 +1,40 @@
+﻿using CartAPI.Data;
+using CartAPI.Models;
+using CartAPI.Services.Interfaces;
+
+namespace CartAPI.Services
+{
+    public class CartService : ICartService
+    {
+        private readonly CartDbContext _cartDbContext;
+        public CartService(CartDbContext cartDbContext)
+        {
+            _cartDbContext = cartDbContext;
+        }
+        public void AddCartItem(Cart item)
+        {
+            _cartDbContext.Carts.Add(item);
+            _cartDbContext.SaveChanges();
+        }
+
+        public void RemoveCartItem(int itemId)
+        {
+            var cartItem = _cartDbContext.Carts.Find(itemId);
+            if (itemId != null)
+            {
+                _cartDbContext.Carts.Remove(cartItem);
+                _cartDbContext.SaveChanges();
+            }
+        }
+
+        public Cart GetCartItem(int itemId)
+        {
+            return _cartDbContext.Carts.Find(itemId);
+        }
+
+        public IEnumerable<Cart> ListCartItems()
+        {
+            return _cartDbContext.Carts.ToList();
+        }
+    }
+}
--- CartAPI/Services/Interfaces/ICartService.cs
+++ CartAPI/Services/Interfaces/ICartService.cs
@@ -4,9 +4,9 @@ namespace CartAPI.Services.Interfaces
 {
     public interface ICartService
     {
-        void AddCartItem(CartDto item);
+        void AddCartItem(Cart item);
         void RemoveCartItem(int itemId);
-        IEnumerable<CartDto> ListCartItems();
-        CartDto GetCartItem(int itemId);
+        IEnumerable<Cart> ListCartItems();
+        Cart GetCartItem(int itemId);
     }
 }
