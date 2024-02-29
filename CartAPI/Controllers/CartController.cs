18fc29589216a5065f0f25c605c0e6c8b5c763a9
AzureYakubu <obsmediaofficial@gmail.com>
2024-02-29 23:01:35 +0000

    implement CRUD operations through Cart Service

--- CartAPI/Controllers/CartController.cs
+++ CartAPI/Controllers/CartController.cs
@@ -1,4 +1,5 @@
-﻿using CartAPI.Services.Interfaces;
+﻿using CartAPI.Models;
+using CartAPI.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CartAPI.Controllers
@@ -13,6 +14,46 @@ namespace CartAPI.Controllers
             _cartService = cartService;
         }
 
-        
+        //add cart item
+        [HttpPost("AddCartItem")]
+        public IActionResult AddcartItem(Cart cartItem)
+        {
+            _cartService.AddCartItem(cartItem);
+            return View(cartItem);
+        }
+
+        //remove cart item
+        [HttpDelete("DeleteCartItem/{id}")]
+        public IActionResult DeleteCartItem(int itemId)
+        {
+            var cartItem = _cartService.GetCartItem(itemId);
+            if (cartItem == null)
+            {
+                return NotFound();
+            }
+
+            _cartService.RemoveCartItem(itemId);
+            return Ok("Cart Item Removed");
+        }
+
+        //get cart item
+        [HttpGet("GetCartItem/{id}")]
+        public IActionResult GetCartItem(int itemId)
+        {
+            var cartItem = _cartService.GetCartItem(itemId);
+            if ( cartItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(cartItem);
+        }
+
+        //list cart items
+        [HttpGet("AllCartItems")]
+        public IActionResult ListCartItems()
+        {
+            var cartItems = _cartService.ListCartItems();
+            return Ok(cartItems);
+        }
     }
 }
