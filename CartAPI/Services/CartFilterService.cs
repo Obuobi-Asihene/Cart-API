1f5fcf3c7ff329bd8ff9320c7ac69ac80f31474f
AzureYakubu <obsmediaofficial@gmail.com>
2024-03-03 00:03:10 +0000

    added cart filter service and interface

---
+++ CartAPI/Services/CartFilterService.cs
@@ -0,0 +1,31 @@
+﻿using CartAPI.Models;
+using CartAPI.Services.Interfaces;
+
+namespace CartAPI.Services
+{
+    public class CartFIlterService : ICartFilterService
+    {
+        public IEnumerable<Cart> FilterCartItems(IEnumerable<Cart> cartItems, string? phoneNumber = null, DateTime? CreatedAt = null, int? Quantity = null, string? item = null)
+        {
+            // Apply filters
+            if (!string.IsNullOrEmpty(phoneNumber))
+            {
+                cartItems = cartItems.Where(c => c.PhoneNumber == phoneNumber);
+            }
+            if (CreatedAt.HasValue)
+            {
+                cartItems = cartItems.Where(c => c.CreatedAt >= CreatedAt.Value);
+            }
+            if (Quantity.HasValue)
+            {
+                cartItems = cartItems.Where(c => c.Quantity == Quantity.Value);
+            }
+            if (!string.IsNullOrEmpty(item))
+            {
+                cartItems = cartItems.Where(c => c.ItemName.Contains(item, StringComparison.OrdinalIgnoreCase));
+            }
+
+            return cartItems;
+        }
+    }
+}
---
+++ CartAPI/Services/Interfaces/ICartFilterService.cs
@@ -0,0 +1,9 @@
+﻿using CartAPI.Models;
+
+namespace CartAPI.Services.Interfaces
+{
+    public interface ICartFilterService
+    {
+        IEnumerable<Cart> FilterCartItems(IEnumerable<Cart> cartItems, string? phoneNumber = null, DateTime? CreatedAt = null, int? Quantity = null, string? item = null);
+    }
+}
