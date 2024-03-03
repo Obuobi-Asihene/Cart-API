f55e824270dc377038bfcd8e3c376458159a476f
AzureYakubu <obsmediaofficial@gmail.com>
2024-03-03 00:01:30 +0000

    updated migration and database

---
+++ CartAPI/Migrations/20240302230419_UpdatedMigration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CartAPI.Migrations
+{
+    public partial class UpdatedMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Carts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Carts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Carts");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Carts");
+        }
+    }
+}
