7186f4542c1cbe293b648c8105bf9b4ebd7e76ca
AzureYakubu <obsmediaofficial@gmail.com>
2024-03-03 23:20:21 +0000

    added item service

---
+++ CartAPI/Services/ItemService.cs
@@ -0,0 +1,30 @@
+﻿using CartAPI.Data;
+using CartAPI.Models;
+using CartAPI.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CartAPI.Services
+{
+    public class ItemService : IItemService
+    {
+        private readonly CartDbContext _context;
+
+        public ItemService(CartDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Item> CreateItemAsync(Item item)
+        {
+            _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+
+            return item;
+        }
+
+        public async Task<Item> GetSingleItemAsync(int id)
+        {
+            return await _context.Items.FirstOrDefaultAsync(i => i.Id == id);
+        }
+    }
+}
