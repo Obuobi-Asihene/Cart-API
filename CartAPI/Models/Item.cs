78e744da7cbdb5d396657e45fed89790c3607adb
AzureYakubu <obsmediaofficial@gmail.com>
2024-03-03 21:44:40 +0000

    added item model

---
+++ CartAPI/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿namespace CartAPI.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Cart Cart { get; set; }
+    }
+}
