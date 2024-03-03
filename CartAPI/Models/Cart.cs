92b3f402eb6bf1e68a4b43256f7904a831d0e18c
AzureYakubu <obsmediaofficial@gmail.com>
2024-03-03 23:22:07 +0000

    added phoneNumber property

--- CartAPI/Models/Cart.cs
+++ CartAPI/Models/Cart.cs
@@ -9,6 +9,7 @@ namespace CartAPI.Models
         public string ItemName { get; set; }
         public int Quantity {  get; set; }
         public decimal UnitPrice {  get; set; }
+        public string PhoneNumber { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
