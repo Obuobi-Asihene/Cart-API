a7a8ef8139a8c619b31f1497e6fdd8c64dba98c5
AzureYakubu <obsmediaofficial@gmail.com>
2024-03-03 00:04:54 +0000

    injected Cart filter interface

--- CartAPI/Controllers/CartController.cs
+++ CartAPI/Controllers/CartController.cs
@@ -9,9 +9,11 @@ namespace CartAPI.Controllers
     public class CartController : Controller
     {
         private readonly ICartService _cartService;
-        public CartController(ICartService cartService)
+        private readonly ICartFilterService _filterService;
+        public CartController(ICartService cartService, ICartFilterService filterService)
         {
             _cartService = cartService;
+            _filterService = filterService;
         }
 
         //add cart item
@@ -50,9 +52,12 @@ namespace CartAPI.Controllers
 
         //list cart items
         [HttpGet("AllCartItems")]
-        public IActionResult ListCartItems()
+        public IActionResult ListCartItems(string? phoneNumber = null, DateTime? CreatedAt = null, int? Quantity = null, string? item = null)
         {
             var cartItems = _cartService.ListCartItems();
+
+            var filterCartItems = _filterService.FilterCartItems(cartItems, phoneNumber, CreatedAt, Quantity, item);                                              
+            
             return Ok(cartItems);
         }
     }
