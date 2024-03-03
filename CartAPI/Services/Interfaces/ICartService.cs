6338de54564971cc9ad858d2edf20cb2bf1ef50d
AzureYakubu <obsmediaofficial@gmail.com>
2024-03-03 23:19:45 +0000

    modified AddCartItem method to accept necessary parameters to add item to cart

--- CartAPI/Services/Interfaces/ICartService.cs
+++ CartAPI/Services/Interfaces/ICartService.cs
@@ -4,9 +4,8 @@ namespace CartAPI.Services.Interfaces
 {
     public interface ICartService
     {
-        void AddCartItem(Cart item);
+        void AddCartItem(int itemId, int quantity, string phoneNumber);
         void RemoveCartItem(int itemId);
         IEnumerable<Cart> ListCartItems();
-        Cart GetCartItem(int itemId);
     }
 }
