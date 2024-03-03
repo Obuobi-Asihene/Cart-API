5a9d728d6ae67d69f9f324130e1b28b0dd965257
AzureYakubu <obsmediaofficial@gmail.com>
2024-03-03 21:45:20 +0000

    added navigation property for item

--- CartAPI/Models/Cart.cs
+++ CartAPI/Models/Cart.cs
@@ -9,8 +9,8 @@ namespace CartAPI.Models
         public string ItemName { get; set; }
         public int Quantity {  get; set; }
         public decimal UnitPrice {  get; set; }
-        public string PhoneNumber { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
-        public DateTime CreatedAt = DateTime.UtcNow;
+        public Item Item { get; set; }
     }
 }
